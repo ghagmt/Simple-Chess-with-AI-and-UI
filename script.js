c257c962e80673a260cb056b3c84b1ee0174544c
ghagmt <g.hagmt@gmail.com>
2019-11-13 17:27:04 +0200

    Adding some files, checking how git works.

---
+++ script.js
@@ -0,0 +1,10 @@
+$.getScript('draw_functions.js', function() {
+$.getScript('help_functions.js', function() {
+$.getScript('analyze_functions.js', function() {
+$.getScript('getValidMoves.js', function() {
+$.getScript('controller.js', function() {
+$.getScript('config.js', function() {
+
+	controller();
+
+});});});});});});
